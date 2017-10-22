d9789837823c88afe57cfa6f84a8ebb0317335fc
rfk1ll <rfk1lldev@gmail.com>
2017-10-22 15:55:51 -0500

    added buffer output

--- air-fetch.cpp
+++ air-fetch.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string>
+#include <unistd.h> // for sleep()
 using namespace std;
 
 //function to utilize iwconfig
@@ -23,35 +24,64 @@ string GetStdoutFromCommand(string cmd) {
 }
 
 //function to check for monitor mode
-void MonitorMode(string ls) {
+int MonitorMode(string ls) {
 
   //string str ();
   string str2 ("Monitor");
 
   // different member versions of find in the same order as above:
   size_t found = ls.find(str2);
-  if (found!=string::npos)
-  cout << R"(
+  if (found!=string::npos){
+      cout << R"(
           _            __      _       _
      __ _(_)_ __      / _| ___| |_ ___| |__
     / _` | | '__|____| |_ / _ \ __/ __| '_ \
    | (_| | | | |_____|  _|  __/ || (__| | | |
     \__,_|_|_|       |_|  \___|\__\___|_| |_|
 
-          }}}}} INJECTION READY {{{{{
-)" << endl;
+        )" << endl;
+        for (;;) {
+            cout << "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b}****INJECTION MODE****{ " << flush;
+            sleep(1);
+            cout << "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b}}***INJECTION MODE***{{ " << flush;
+            sleep(1);
+            cout << "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b}}}**INJECTION MODE**{{{ " << flush;
+            sleep(1);
+            cout << "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b}}}}*INJECTION MODE*{{{{ " << flush;
+            sleep(1);
+            cout << "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b}}}}}INJECTION MODE{{{{{ " << flush;
+            sleep(1);
+            cout << "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b}}}}}INJECTION MODE{{{{{ " << endl;
 
-  else
-  cout << R"(
+            return 0;
+        }
+    }
+    else{
+        cout << R"(
           _            __      _       _
      __ _(_)_ __      / _| ___| |_ ___| |__
     / _` | | '__|____| |_ / _ \ __/ __| '_ \
    | (_| | | | |_____|  _|  __/ || (__| | | |
     \__,_|_|_|       |_|  \___|\__\___|_| |_|
 
-            }}}}} STEALTH MODE {{{{{
-)" << endl;
+        )" << endl;
+        for (;;) {
+             cout << "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b}****PASSIVE MODE****{ " << flush;
+             sleep(1);
+             cout << "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b}}***PASSIVE MODE***{{ " << flush;
+             sleep(1);
+             cout << "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b}}}**PASSIVE MODE**{{{ " << flush;
+             sleep(1);
+             cout << "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b}}}}*PASSIVE MODE*{{{{ " << flush;
+             sleep(1);
+             cout << "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b}}}}}PASSIVE MODE{{{{{ " << flush;
+             sleep(1);
+             cout << "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b}}}}}PASSIVE MODE{{{{{ " << endl;
 
+             return 0;
+
+        }
+    }
 }
 
 int main (){
